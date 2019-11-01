0a6103eb1d622713a029dab6e17ff3d8b649e47e
debayandc <debayan.dc31@gmail.com>
2019-11-01 18:33:00 +0530

    docs folder

---
+++ docs/precache-manifest.e12eb01abaec9db53f36d49efa855d63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95a159c0ce3525a9658c5f00f6fc376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36dcf2fa6681415447e8",
+    "url": "/static/css/main.5b17e772.chunk.css"
+  },
+  {
+    "revision": "511ed76be7ec78e3b995",
+    "url": "/static/js/2.1d4a46ce.chunk.js"
+  },
+  {
+    "revision": "36dcf2fa6681415447e8",
+    "url": "/static/js/main.3800bda7.chunk.js"
+  },
+  {
+    "revision": "434872893da42ccf6f6f",
+    "url": "/static/js/runtime-main.9f9cf6c2.js"
+  }
+]);
